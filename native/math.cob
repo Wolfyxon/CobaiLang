da07ebac4240ae73c81a16eb60f3932f6caa6a2b
Wolfyxon <wolfyxon@gmail.com>
2024-09-17 16:11:34 +0200

    Init native math library

---
+++ native/math.cob
@@ -0,0 +1,21 @@
+public const PI: num = 3.14159265359
+
+public func abs(number: num) num:
+        if n < 0:
+            return -n
+        end
+
+        return n
+end
+
+public func clamp(value: num, min: num, max: num) num:
+    if value < min:
+        return min
+    end
+
+    if value > max:
+        return max
+    end
+    
+    return n
+end
